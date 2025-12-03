9913048a53d16e3f7772cfaafe06cb0caed45091
ReiDelsAltres <velibecovv@gmail.com>
2025-12-04 01:37:49 +0400

    i

--- out/404.html.js
+++ out/404.html.js
@@ -1,5 +1,4 @@
 import { Router, HOSTING_ORIGIN } from "@Purper";
-let fff = window.location;
 Router.savePersistedRoute(new URL(window.location.href, HOSTING_ORIGIN));
 Router.legacyRouteTo("index.html");
 //# sourceMappingURL=404.html.js.map
