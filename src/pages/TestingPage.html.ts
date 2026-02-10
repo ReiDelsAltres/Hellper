b7bc56f81946a82265eaf04c6457b2429171b62e
BpYuska <yusif.safarov.2007@mail.ru>
2026-02-10 15:22:06 +0400

    Update TestingPage.html.ts

--- src/pages/TestingPage.html.ts
+++ src/pages/TestingPage.html.ts
@@ -34,7 +34,7 @@ export default class TestingPage extends Page {
     ]),
     new Semestr("?", [
       new Subject("Vergi qanunvericiliyi?", "?", ["No related"], "Vergi qanunvericiliyi","s_no_idea/qan.json"),
-      new Subject("XH nezeri?", "?", ["No related"], "XH nezeri","s_no_idea/qan.json")
+      new Subject("XH nezeri?", "?", ["No related"], "XH nezeri","s_no_idea/qan1.json")
     ])
   ];
   protected async preInit(): Promise<void> {
